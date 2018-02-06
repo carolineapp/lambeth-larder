aff5a8d27ecf96e62ab0a634e60f24899dd7201c
caralemony <carolinelappleby@gmail.com>
2018-02-06 11:10:35 +0000

    added description

--- server/database/airtable.js
+++ server/database/airtable.js
@@ -1,21 +1,21 @@
 require('env2')('./config.env');
 const API_KEY = process.env.API_KEY;
+const API_BASE = process.env.API_BASE
 const Airtable = require('airtable');
 Airtable.configure({
     endpointUrl: 'https://api.airtable.com',
     apiKey: API_KEY
 });
-const base = Airtable.base('appiS0nCpeGPbRrIK');
+const base = Airtable.base(API_BASE);
 
 base('Times and Venues').select({
-    // Selecting the first 3 records in Grid view:
     maxRecords: 100,
     view: "Grid view"
 }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
 
     records.forEach(function(record) {
-        console.log(record.get('Name'));
+        console.log(record.get('Name'), record.get('Description'));
     });
 
     // To fetch the next page of records, call `fetchNextPage`.
