a465fe5b099477bae4a827e38936be20e06d03b4
caralemony <carolinelappleby@gmail.com>
2018-02-07 17:00:04 +0000

    moving state to app.js

--- client/src/components/search/SearchForm.js
+++ client/src/components/search/SearchForm.js
@@ -1,58 +1,14 @@
 import React, { Component } from "react";
 
-class SearchForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      postcode: "",
-      timeOption: "",
-      adviceCentres: false
-    };
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      postcode: event.target.value
-    });
-    console.log(this.state);
-  }
-
-  handleTime = (event) => {
-    this.setState({
-      timeOption: event.target.value
-    });
-    console.log(this.state);
-  }
-
-  toggleAdviceCentres = (event) => {
-    this.setState({
-      adviceCentres: !this.state.adviceCentres
-    });
-    console.log(this.state);
-  }
-
-  checkPostcode = e => {
-    e.preventDefault();
-    console.log(this.state.postcode);
-    fetch(`https://api.postcodes.io/postcodes/${this.state.postcode}`)
-         .then(response => response.json()
-             )
-            .then(data => console.log(data)
-          )
-          .catch(error => console.log('error is', error))
-
-  }
-
-
-  render() {
+const SearchForm = props => {
     return (
       <form>
         <input
           type="text"
           name="postcode"
           placeholder="Enter your postcode"
-          value={this.state.postcode}
-          onChange={this.handleChange}
+          value={props.postcode}
+          onChange={props.handleChange}
         />
         <p>When do you need food?</p>
         <label for="time">Today</label>
@@ -60,36 +16,35 @@ class SearchForm extends Component {
           type="radio"
           name="time"
           value="today"
-          checked={this.state.timeOption === "today"}
-          onChange={this.handleTime}
+          checked={props.timeOption === "today"}
+          onChange={props.handleTime}
         />
         <label for="time">Tomorrow</label>
         <input
           type="radio"
           name="time"
           value="tomorrow"
-          checked={this.state.timeOption === "tomorrow"}
-          onChange={this.handleTime}
+          checked={props.timeOption === "tomorrow"}
+          onChange={props.handleTime}
         />
         <label for="time">Later</label>
         <input
           type="radio"
           name="time"
           value="later"
-          checked={this.state.timeOption === "later"}
-          onChange={this.handleTime}
+          checked={props.timeOption === "later"}
+          onChange={props.handleTime}
         />
         <br />
         <label for="advice-centres">See advice centres</label>
         <input
           type="checkbox"
           name="advice-centres"
-          onChange={this.toggleAdviceCentres}
+          onChange={props.toggleAdviceCentres}
         /><br />
-        <button type="submit" onClick={ this.checkPostcode }>Go</button>
+        <button type="submit" onClick={ props.checkPostcode }>Go</button>
       </form>
     );
-  }
 }
 
 export default SearchForm;
