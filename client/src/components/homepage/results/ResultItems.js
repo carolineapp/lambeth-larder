81a31252d7c054fb45ea46dddf0a076785f5f6a0
parkhabit <parkhabit@gmail.com>
2018-02-12 14:25:21 +0000

    results displaying in list

--- client/src/components/homepage/results/ResultItems.js
+++ client/src/components/homepage/results/ResultItems.js
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 const ResultItems = ({ ...props }) => {
+  const noResult =
+    "! No emergency food venues are open in Lambeth now. Try searching for later this week or alternatively call One Lambeth Advice on 0800 254 0298.";
+
   let flatten = [];
   props.result
     ? props.result.map(a => {
         flatten.push([a.fields.Name, a.fields.Description]);
       })
-    : "empty";
-  console.log(flatten);
+    : { noResult };
+
   return (
     <ul className="results">
-      {flatten.map(item => <li key={item}>{item}</li>)}
+      {flatten.length > 1
+        ? flatten.map(item => <li key={item}>{item}</li>)
+        : noResult}
     </ul>
   );
 };
