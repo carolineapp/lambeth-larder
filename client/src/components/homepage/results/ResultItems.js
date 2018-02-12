15d530d3d24e7dde55d5012664c9563dd371da69
parkhabit <parkhabit@gmail.com>
2018-02-12 10:36:41 +0000

    removed annon function that calls handler relates to issue #26

---
+++ client/src/components/homepage/results/ResultItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+const ResultItems = ({ ...props }) => {
+  console.log(props);
+  return (
+    <div>
+      <div>hello</div>
+    </div>
+  );
+};
+
+export default ResultItems;
