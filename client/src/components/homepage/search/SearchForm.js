53a5df06d27d5e4ee293f74852278518d910e4ef
parkhabit <parkhabit@gmail.com>
2018-02-13 17:59:47 +0000

    changed background color

--- client/src/components/homepage/search/SearchForm.js
+++ client/src/components/homepage/search/SearchForm.js
@@ -1,9 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+
+const FormStyle = styled.form`
+  display: flex;
+  background-color: #e71242;
+  color: white;
+  flex-direction: column;
+`;
+const RadioButtonContainer = styled.div`
+  display: inherit;
+  flex-direction: row;
+`;
+const PostcodeSearchBar = styled.input`
+  display: flex;
+  align-self: center;
+  width: 80vw;
+  height: 6vh;
+  background-color: #e71242;
+  font: lato;
+  color: white;
+  font-size: 18px;
+  &::placeholder {
+    color: white;
+  }
+`;
+const Radio = styled.input`
+  width: 20vw;
+  height: 4vh;
+  padding: 2%;
+  color: white;
+  background-color: #e71242;
+  border: 2px solid white;
+`;
 
 const SearchForm = ({ ...props }) => {
   return (
-    <form>
-      <input
+    <FormStyle>
+      <PostcodeSearchBar
         type="text"
         name="postcode"
         placeholder="Enter your postcode"
@@ -11,27 +44,29 @@ const SearchForm = ({ ...props }) => {
         onChange={props.handleChange}
       />
       <p>When do you need food?</p>
-      <label htmlFor="time">Today</label>
-      <input
-        type="radio"
-        name="time"
-        value="today"
-        onChange={props.handleTime}
-      />
-      <label htmlFor="time">Tomorrow</label>
-      <input
-        type="radio"
-        name="time"
-        value="tomorrow"
-        onChange={props.handleTime}
-      />
-      <label htmlFor="time">Later</label>
-      <input
-        type="radio"
-        name="time"
-        value="later"
-        onChange={props.handleTime}
-      />
+      <RadioButtonContainer>
+        <label htmlFor="time">Today</label>
+        <Radio
+          type="radio"
+          name="time"
+          value="today"
+          onChange={props.handleTime}
+        />
+        <label htmlFor="time">Tomorrow</label>
+        <Radio
+          type="radio"
+          name="time"
+          value="tomorrow"
+          onChange={props.handleTime}
+        />
+        <label htmlFor="time">Later</label>
+        <Radio
+          type="radio"
+          name="time"
+          value="later"
+          onChange={props.handleTime}
+        />
+      </RadioButtonContainer>
       <br />
       <label htmlFor="advice-centres">See advice centres</label>
       <input
@@ -43,7 +78,7 @@ const SearchForm = ({ ...props }) => {
       <button type="submit" onClick={props.checkPostcode}>
         Go
       </button>
-    </form>
+    </FormStyle>
   );
 };
 
