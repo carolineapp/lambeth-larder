0106b09f574e4a01256b9118e1806af83256d7e8
Lex <alexisjaynedefoe@gmail.com>
2018-02-15 21:33:43 +0000

    :lipstick: delete console.logs

--- client/src/components/voucher/Voucher.js
+++ client/src/components/voucher/Voucher.js
@@ -50,12 +50,7 @@ const Button = styled.button`
 `;
 
 const Voucher = props => {
- //Can we add a loading message/spinner here?
-  // if (!props) {
-  //   return <div>Loading...</div>;
-  // }
 
-console.log(props)
   return (
     <Div>
       <Navbar />
