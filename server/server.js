abf6b937ccf1839e322c3afb0793abf9478ddf0a
caralemony <carolinelappleby@gmail.com>
2018-02-22 21:14:05 +0000

    adding things

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.disabled("x-powered-by");
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "..", "client", "build")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
   });
 }
