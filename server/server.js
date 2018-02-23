1a2e8b17bfaef50116565b25dbd0d52bf76ceabf
caralemony <carolinelappleby@gmail.com>
2018-02-23 14:40:21 +0000

    added node env to package.json

--- server/server.js
+++ server/server.js
@@ -11,10 +11,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
 app.disabled("x-powered-by");
 
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
